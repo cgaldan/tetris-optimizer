850cace511518fe42542dd0a3d6d3c7b33daa0d7
cgkaldan <chrisgaldan@gmail.com>
2025-01-25 06:32:06 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	tetro "tetris/utils"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <filename>")
+		return
+	}
+
+	filename := os.Args[1]
+	tetrominoes, err := tetro.ReadTetrominoes(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i, t := range tetrominoes {
+		fmt.Printf("Tetromino %d:\n", i+1)
+		for _, line := range t {
+			fmt.Println(string(line[:]))
+		}
+		fmt.Println()
+	}
+}
---
+++ utils/tetrominoes.go
@@ -0,0 +1,123 @@
+package tetro
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ReadTetrominoes(filename string) ([][4][4]rune, error) {
+	filename = "examples/" + filename
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var tetrominoes [][4][4]rune
+	var current []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "" {
+			if len(current) > 0 {
+				tetromino, err := formatCheck(current)
+				if err != nil {
+					return nil, err
+				}
+
+				tetrominoes = append(tetrominoes, tetromino)
+				current = nil
+			}
+			continue
+		}
+
+		current = append(current, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	if len(current) > 0 {
+		tetromino, err := formatCheck(current)
+		if err != nil {
+			return nil, err
+		}
+		tetrominoes = append(tetrominoes, tetromino)
+	}
+
+	return tetrominoes, nil
+}
+
+func formatCheck(lines []string) ([4][4]rune, error) {
+	var tetromino [4][4]rune
+
+	if len(lines) != 4 {
+		return tetromino, fmt.Errorf("tetromino must have 4 lines")
+	}
+
+	for i, line := range lines {
+		if len(line) != 4 {
+			return tetromino, fmt.Errorf("line %d must have 4 characters", i+1)
+		}
+
+		for j, char := range line {
+			if char != '.' && char != '#' {
+				return tetromino, fmt.Errorf("line %d character %d must be '.' or '#'", i+1, j+1)
+			}
+			tetromino[i][j] = char
+		}
+	}
+
+	if !tetrominoCheck(tetromino) {
+		return tetromino, fmt.Errorf("ERROR: tetromino is not valid")
+	}
+
+	return tetromino, nil
+}
+
+func tetrominoCheck(tetromino [4][4]rune) bool {
+	visited := [4][4]bool{}
+	var blocks [][2]int
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if tetromino[i][j] == '#' {
+				blocks = append(blocks, [2]int{i, j})
+			}
+		}
+	}
+
+	if len(blocks) != 4 {
+		return false
+	}
+
+	var queue [][2]int
+	queue = append(queue, blocks[0])
+	visited[blocks[0][0]][blocks[0][1]] = true
+
+	count := 0
+	directions := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		count++
+
+		for _, dir := range directions {
+			ni, nj := current[0]+dir[0], current[1]+dir[1]
+
+			if ni >= 0 && ni < 4 && nj >= 0 && nj < 4 && tetromino[ni][nj] == '#' && !visited[ni][nj] {
+				visited[ni][nj] = true
+				queue = append(queue, [2]int{ni, nj})
+			}
+		}
+	}
+
+	return count == 4
+}
